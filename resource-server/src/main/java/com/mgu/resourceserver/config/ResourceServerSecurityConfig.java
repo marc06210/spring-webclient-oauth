809858a438ae76ad35d7540c2d06a8ee6eb7b4aa
Marc Guerrini <maguerrini@airfrance.fr>
2023-01-11 11:19:55 +0100

    updated to SB 3

--- resource-server/src/main/java/com/mgu/resourceserver/config/ResourceServerSecurityConfig.java
+++ resource-server/src/main/java/com/mgu/resourceserver/config/ResourceServerSecurityConfig.java
@@ -1,19 +1,18 @@
 package com.mgu.resourceserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class ResourceServerSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .mvcMatcher("/messages/**")
-                .authorizeRequests()
-                .mvcMatchers("/messages/**").access("hasAuthority('SCOPE_message.read')")
-                .and()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/bikes").hasAuthority("SCOPE_message.read"))
                 .oauth2ResourceServer()
                 .jwt();
         return http.build();
